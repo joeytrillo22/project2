a9bcc84a2e19afbcc5fc6e19277db3a8d1c918ff
Joseph Trillo <josephtrillo@Josephs-MacBook-Pro.local>
2020-05-02 14:04:25 -0700

    trying to get search engine to run

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 //API Key 49tPTYHGFwcqLv0N15BPgc425XU6C48E
 
+// URL https://api.giphy.com/v1/gifs/search?api_key=49tPTYHGFwcqLv0N15BPgc425XU6C48E&q=&limit=10&offset=0&rating=PG&lang=en
+
+$(document).ready(()=>{
+    $("#submit").click(()=>{
+        let userinput = $("#search").val()
+        //alert(userinput)
+    })
+    $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?api_key=49tPTYHGFwcqLv0N15BPgc425XU6C48E&q=${userinput}&limit=10&offset=0&rating=PG&lang=en`
+    })
+    .done((res)=>{
+        console.log("res", res)
+     
+    })
+})
